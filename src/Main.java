1e909328c6a838556c76026f591aa62ceb5cd26e
Nate Peyer <nathaniel.peyer24@d155.org>
2023-11-29 20:36:24 -0600

    This is updated at home

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.println("test");
+        int dog = scan.nextInt();
     }
 }
